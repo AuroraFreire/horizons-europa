f3da474d61ec579e64122bde229490f624027a77
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-07-25 18:00:49 +0200

    make player slower when low on energy

--- player.gd
+++ player.gd
@@ -5,6 +5,7 @@ extends Node2D
 var lane_index = 0
 var position_offset: Vector2
 var target_pos
+@onready var sun = get_node("../Sun")
 
 func _ready():
 	position_offset = position
@@ -23,13 +24,14 @@ func _process(delta: float) -> void:
 	target_pos = lane_index*lanes_distance+position_offset.x
 	
 	# smoothly lerp to new target position
-	position.x = lerp(position.x,target_pos,1.0-0.01**(delta))
+	var lerp_speed = 0.01 + 0.02 * (1.0-sun.counter)
+	position.x = lerp(position.x,target_pos,1.0-lerp_speed**(delta))
+	var const_speed = 100 - 80 * (1.0-sun.counter)
 	# move towards new position with fixed speed too
 	if target_pos > position.x:
-		position.x = min(position.x+delta*100,target_pos)
+		position.x = min(position.x+delta*const_speed,target_pos)
 	if target_pos < position.x:
-		position.x = max(position.x-delta*100,target_pos)
-
+		position.x = max(position.x-delta*const_speed,target_pos)
 
 func _on_area_2d_area_entered(_area: Area2D) -> void:
 	print("die")
