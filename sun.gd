9b53b19a1459d32d54c0a9b2598a79cfeaf746bf
Liam carpello <cyberknight123@proton.me>
2026-07-25 16:36:31 +0100

    added kinda counter

--- sun.gd
+++ sun.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 var timer = randf_range(500, 1000) 
 var time = 0
-@export var lanes_distance = 90.0
+var counter = 1
 @export var lanes_amount = 6
+@onready var player = get_node("../Player")
+@onready var sun = get_node("../Sun")
 var lane_index = 0
 var position_offset: Vector2
 var target_pos
@@ -18,12 +20,18 @@ func _process(_delta: float) -> void:
 	pass
 	time += 2
 	if(time >= timer):
-		var lane_index = randi_range(0, 5)
-		var lanepos = lane_index * lanes_distance - 242.0
+		lane_index = randi_range(0, 5)
+		var lanepos = lane_index * player.lanes_distance - 242.0
 		position.x = (lanepos)
 		time = 0
-		timer == randf_range(500, 1000)
+		timer = randf_range(500, 1000)
 	elif (time != timer):
 		print(timer)
 		print(time)
-		
+	if(player.lane_index != sun.lane_index): 
+		for counter in 10000000:
+			counter -= 0.001
+			if(counter <= 0):
+				print("dead")
+	elif(player.lane_index == sun.lane_index):
+		counter += 1 
