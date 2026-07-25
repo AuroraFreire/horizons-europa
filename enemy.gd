6c7bf82ea6c094ba455c93c8842e6f781b1c27ec
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-07-25 16:02:48 +0200

    add raven texture and adjust sunbeam

--- enemy.gd
+++ enemy.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
-@export var random_sprites: Array[Texture2D]
+@export var down_texture: Texture2D
 @export var enemy_speed = 40.0
 var lane_index = 0
 var start_pos = 0
 
 func _ready() -> void:
-	if len(random_sprites) > 0:
-		$Texture.texture = random_sprites[randi_range(0,len(random_sprites)-1)]
+	if lane_index % 2 == 0:
+		$Texture.texture = down_texture
 
 func _process(delta: float) -> void:
 	var mod = 1 if lane_index % 2 == 0 else -1
