0385b36bc65dec5a97e95da32062a5d643100d50
AuroraFreire <lourencofreire3@gmail.com>
2026-07-26 06:42:35 +0200

    main menu finishedgit add .

--- main_menu.gd
+++ main_menu.gd
@@ -3,9 +3,14 @@ extends Control
 @onready var videoBg = $VideoStreamPlayer
 @onready var anim = $AnimationPlayer
 @onready var anim2 = $AnimationPlayer2
+@onready var play = $play
+@onready var options = $options
+@onready var options2 = $options2
+@onready var exit = $exit
+@onready var title = $RichTextLabel
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	options2.visible = false
 	videoBg.play()
 	anim.play("button_entrance")
 	anim2.play("title_entrance")
@@ -13,6 +18,21 @@ func _ready() -> void:
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://world.tscn")
 
-
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_options_pressed() -> void:
+	options2.visible = true
+	exit.visible = false
+	play.visible = false
+	options.visible = false
+	title.visible = false
+
+
+func _on_button_pressed() -> void:
+	options2.visible = false
+	exit.visible = true
+	play.visible = true
+	options.visible = true
+	title.visible = true
